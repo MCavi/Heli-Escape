9c8f3cede68e1d9bcba5c271a3e5543a7f43a5d9
Marcus <mcavini@outlook.com>
2019-04-13 11:16:21 -0400

    Display score in canvas

--- src/javascript/spaceship.js
+++ src/javascript/spaceship.js
@@ -7,10 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const ctx = canvas.getContext('2d');
     const ship = new Ship(ctx);
     const gates = [];
-    const interval = setInterval(game, 30);
+    const interval = setInterval(gameEngine, 30);
     gates[0] = new Gate(ctx);
     const background = new Image();
     background.src = "../assets/space-background.jpeg"
+    let score = 0;
 
     // Adds new gate once previous gate reaches 400px.
     // Removes gate once it leaves the bounds of the canvas.
@@ -28,6 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // MAIN HIT DETECTION
     function hitDetected(){
+        // debugger
+        if ( ship.y == gates[0].y ) {
+            score += 1;
+        }
         if ( isHittingWall() || isHittingGate() ) {
             gameOver();
         }
@@ -37,6 +42,15 @@ document.addEventListener('DOMContentLoaded', () => {
     function isHittingWall(){
         if (ship.x < -2 || ship.x > canvas.width - 35) {
             return true;
+        }
+    }
+
+    // Gate hit detection
+    function isHittingGate() {
+        for ( let i = 0; i < gates.length; i++ ) {
+            if (isHittingLeftGate(i) || isHittingRightGate(i)) {
+                return true ;
+            }
         }
     }
 
@@ -60,32 +74,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Gate hit detection
-    function isHittingGate() {
-        for ( let i = 0; i < gates.length; i++ ) {
-            if (isHittingLeftGate(i) || isHittingRightGate(i)) {
-                return true ;
-            }
-        }
-    }
-
-
-
-    // MAIN GAME FUNCTION
-    function game() {
-        hitDetected();
-        ctx.clearRect( 0, 0, canvas.width, canvas.height );
-        ship.draw();
-        checkGate();
-        isHittingGate();
-    }
-
     function gameOver(){
         alert("GAME OVER");
         document.location.reload();
         clearInterval(interval);
     }
 
-    
+    function drawScore() {
+        ctx.font = "48px sans serif";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText(score, 275, 50);
+    }
 
+
+    // MAIN GAME FUNCTION
+    function gameEngine() {
+        hitDetected();
+        ctx.clearRect( 0, 0, canvas.width, canvas.height );
+        ship.draw();
+        drawScore();
+        checkGate();
+    }
 })
